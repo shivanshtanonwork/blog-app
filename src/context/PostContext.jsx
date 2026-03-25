c6f4d6573e4940a204818642480933af5be80dd3
shivanshtanonwork <shivanshtandonwork@gmail.com>
2026-03-25 22:30:30 +0530

    Refactor PostContext to persist posts in localStorage and initialize state from it; add useEffect for synchronization.

--- src/context/PostContext.jsx
+++ src/context/PostContext.jsx
@@ -1,20 +1,34 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const PostContext = createContext();
 
+// localStorage.getItem("posts");
+// localStorage.setItem("posts");
+
 function PostProvider(props) {
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: "Hello",
-      content: "Welcome to my new Blog",
-    },
-    {
-      id: 2,
-      title: "Hello World",
-      content: "Welcome to my 2nd Blog",
-    },
-  ]);
+  const [posts, setPosts] = useState(() => {
+    const storedPost = localStorage.getItem("posts");
+    if (storedPost) {
+      return JSON.parse(storedPost);
+    } else {
+      return [
+        {
+          id: 1,
+          title: "Hello",
+          content: "Welcome to my new Blog",
+        },
+        {
+          id: 2,
+          title: "Hello World",
+          content: "Welcome to my 2nd Blog",
+        },
+      ];
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem("posts", JSON.stringify(posts));
+  }, [posts]);
 
   function addPost(newPost) {
     const lastId = posts[posts.length - 1].id;
